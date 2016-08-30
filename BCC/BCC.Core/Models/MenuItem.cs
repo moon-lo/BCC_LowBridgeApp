1667908d0e5b8c0afbbb8fb5210b0c7a5e0a9230
Moon <moonxdd@gmail.com>
2016-08-31 09:15:10 +1000

    Hamburger menu added
    still needs logic for event listener

---
+++ BCC/BCC.Core/Models/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace BCC.Core.Models
+{
+    public class MenuItem
+    {
+        public string Title { get; set; }
+
+        public Type ViewModelType { get; set; }
+    }
+}
---
+++ BCC/BCC.Core/ViewModels/HamburgerViewModel.cs
@@ -0,0 +1,40 @@
+﻿using BCC.Core.Models;
+using MvvmCross.Core.ViewModels;
+using System.Collections.ObjectModel;
+
+namespace BCC.Core.ViewModels
+{
+    public class HamburgerViewModel
+        : MvxViewModel
+    {
+        
+        public ObservableCollection<MenuItem> MenuItems { get; private set; }
+
+        private MvxCommand<MenuItem> itemSelectedCommand;
+
+        public IMvxCommand ItemSelectedCommand
+        {
+            get
+            {
+                itemSelectedCommand = itemSelectedCommand ?? new MvxCommand<MenuItem>(MenuAction);
+                return itemSelectedCommand;
+            }
+        }
+
+        public void MenuAction(MenuItem item)
+        {
+            ShowViewModel(item.ViewModelType);
+        }
+
+        public void MenuViewModel()
+        {
+            MenuItems = new ObservableCollection<MenuItem>();
+
+            MenuItems.Add(new MenuItem() { Title = "Settings", ViewModelType = typeof(HamburgerViewModel) });
+            MenuItems.Add(new MenuItem() { Title = "Help", ViewModelType = typeof(HamburgerViewModel) });
+            MenuItems.Add(new MenuItem() { Title = "About, terms & privacy", ViewModelType = typeof(HamburgerViewModel) });
+        }
+    }
+
+}
+
