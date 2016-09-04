681159c25c9adba68a8a8d2eff1f36faeaf26556
michael <michael@michaeldevenish.com>
2016-09-04 21:54:47 +1000

    updated Designs

---
+++ BCC/BCC.Core/ViewModels/VehicleProfilesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCC.Core.ViewModels
+{
+    public class VehicleProfilesViewModel
+       : MvxViewModel
+    {
+
+
+    }
+}
---
+++ BCC/BCC.Droid/Views/VehicleProfilesView.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Views;
+
+namespace BCC.Droid.Views
+{
+    [Activity(Label = "Vehicle Profiles")]
+
+    public class VehicleProfilesView : MvxActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.VehicleProfiles);
+
+        }
+    }
+}
+
