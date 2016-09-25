8479be9f05c7fc0774949b16e224f9530478c723
michael <michael@michaeldevenish.com>
2016-09-25 22:50:34 +1000

    setup active notification

--- BCC/BCC.Droid/Views/LocationService.cs
+++ BCC/BCC.Droid/Views/LocationService.cs
@@ -24,7 +24,14 @@ namespace BCC.Droid.Views
         private LocationManager _locationManager;
         private string _locationProvider;
         public bool inForeground = true;
+        private bool showingActiveAlert = false;
         private List<BridgeData> bridges;
+        private List<BridgeData> notifiedBridges = new List<BridgeData>();//the bridges that the user is close to
+
+        //have a global for the push notification
+        //modify the push when you are close to a bridge if it is already shown
+        //if it is not shown create one
+        //
 
         public List<BridgeData> Bridges { get { return bridges; } set { bridges = value; } }
         public Location CurrentLocation { get { return currentLocation; } set { currentLocation = value; } }
@@ -69,18 +76,39 @@ namespace BCC.Droid.Views
         public void OnLocationChanged(Location location)
         {
             CurrentLocation = location;
+            List<BridgeData> currentNotifiedBridges = new List<BridgeData>(notifiedBridges);
+            bool warning = true;
+            notifiedBridges = new List<BridgeData>();
 
-            bool warning = false;
-            List<BridgeData> warningLocations = new List<BridgeData>();
-
-            warning = CalculateDistanceFromBridges(location, warning, warningLocations);
-
+            warning = CalculateDistanceFromBridges(location, warning, notifiedBridges);
             if (inForeground)
             {
+                if (warning && !showingActiveAlert)
+                {
+                    showingActiveAlert = true;
+                    AlertDialog.Builder alert = new AlertDialog.Builder(binder.activity);
+                    alert.SetTitle("Alert!");
+                    alert.SetMessage("You are approaching a bridge that is too short for your vehicle!");//change this to mention what bridges
+                    alert.SetPositiveButton("Ok", (senderAlert, args) =>
+                    {
+                        showingActiveAlert = false;
+                    });
+                    Dialog dialog = alert.Create();
+                    dialog.Show();
+
+                }
                 binder.activity.updateMap(location);
+
             }
             else
             {
+                if (warning)
+                {
+
+                    //go through the currentNotifiedBridges and see if it is in notified bridges, 
+                    //if it is in both do nothing, if it is in current but not notified remove it from the push
+                    //if it is in notified only add it to the push
+                }
                 Toast.MakeText(this, "closed", ToastLength.Long).Show();
             }
         }
