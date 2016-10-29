8dd31ba9df64c9b732e5cc366404ffbaecbf3348
Moon <moonxdd@gmail.com>
2016-10-29 22:10:01 +1000

    QR scan ViewModel

--- BCC/BCC.Core/ViewModels/ScanViewModel.cs
+++ BCC/BCC.Core/ViewModels/ScanViewModel.cs
@@ -1,13 +1,33 @@
 ﻿using MvvmCross.Core.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using ZXing;
 
 namespace BCC.Core.ViewModels
 {
     public class ScanViewModel : MvxViewModel
     {
+        private ObservableCollection<string> barcodes = new ObservableCollection<string>();        public ObservableCollection<string> Barcodes
+        {
+            get { return barcodes; }
+            set { SetProperty(ref barcodes, value); }
+        }        public ICommand GenerateQRCodeCommand { get; private set; }
+        public ICommand ScanContinuouslyCommand { get; private set; }
+        public IMobileBarcodeScanner scanner;
+
+        public ScanViewModel()
+        {
+            GenerateQRCodeCommand = new MvxCommand<string>(selectedBarcode =>
+            {
+                ShowViewModel<BarcodeViewModel>(new { param = selectedBarcode });
+            });
+            ScanContinuouslyCommand = new MvxCommand(ScanContinuously);
+        }
+
     }
 }
